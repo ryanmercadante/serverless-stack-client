7aa25720de783317cc210dea52dddef1e4d90a74
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2020-12-05 17:52:49 -0500

    add ability to delete a note

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -69,6 +69,10 @@ export default function Notes() {
     }
   }
 
+  function deleteNote() {
+    return API.del('notes', `/notes/${id}`);
+  }
+
   async function handleDelete(event) {
     event.preventDefault();
 
@@ -81,6 +85,14 @@ export default function Notes() {
     }
 
     setIsDeleting(true);
+
+    try {
+      await deleteNote();
+      history.push('/');
+    } catch (err) {
+      onError(err);
+      setIsDeleting(false);
+    }
   }
 
   useEffect(() => {
